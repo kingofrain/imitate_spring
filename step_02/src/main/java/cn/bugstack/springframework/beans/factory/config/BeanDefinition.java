e91544016f9ab045599c86fb42a15a012ba12746
brontosan <838709801@qq.com>
2022-09-08 14:38:41 +0800

    第二次完善

---
+++ step_02/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,22 @@
+package cn.bugstack.springframework.beans.factory.config;
+
+/**
+ * 这是基础类，用来装Class
+ */
+@SuppressWarnings({"rawtypes"})
+public class BeanDefinition {
+
+    private Class beanClass;
+
+    public BeanDefinition(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+}
