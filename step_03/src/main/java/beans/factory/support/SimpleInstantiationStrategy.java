3ab98c6d4cc7a1d443264cb7d9a88e216a04ee82
brontosan <838709801@qq.com>
2022-09-08 18:47:48 +0800

    调用了Enhancer类来实现 没了解过这个类

--- step_03/src/main/java/beans/factory/support/SimpleInstantiationStrategy.java
+++ step_03/src/main/java/beans/factory/support/SimpleInstantiationStrategy.java
@@ -8,6 +8,13 @@ import java.lang.reflect.InvocationTargetException;
 
 /**
  * 这个类实现了 对有参和无参构造2中情况的处理
+ * 首先通过 beanDefinition 获取 Class 信息，这个 Class 信息是在 Bean 定义的时
+ * 候传递进去的。
+ *  接下来判断 ctor 是否为空，如果为空则是无构造函数实例化，否则就是需要有构
+ * 造函数的实例化。
+ *  这里我们重点关注有构造函数的实例化，实例化方式为
+ * clazz.getDeclaredConstructor(ctor.getParameterTypes()).ne
+ * wInstance(args);，把入参信息传递给 newInstance 进行实例化。
  */
 public class SimpleInstantiationStrategy implements InstantiationStrategy{
     @Override
