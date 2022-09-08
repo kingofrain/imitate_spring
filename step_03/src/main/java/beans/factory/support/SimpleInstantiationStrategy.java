e8a01bb17c061412474548a63c66a09b38beb288
brontosan <838709801@qq.com>
2022-09-08 18:38:33 +0800

    有参和无参对应的处理

--- step_03/src/main/java/beans/factory/support/SimpleInstantiationStrategy.java
+++ step_03/src/main/java/beans/factory/support/SimpleInstantiationStrategy.java
@@ -1,4 +1,26 @@
 package beans.factory.support;
 
-public class SimpleInstantiationStrategy {
+import beans.BeansException;
+import beans.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * 这个类实现了 对有参和无参构造2中情况的处理
+ */
+public class SimpleInstantiationStrategy implements InstantiationStrategy{
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor ctor, Object[] args) throws BeansException {
+        Class clazz = beanDefinition.getBeanClass();
+        try {
+            if (null!=ctor){
+                return clazz.getDeclaredConstructor(ctor.getParameterTypes()).newInstance(args);
+            }else {
+                return clazz.getDeclaredConstructor().newInstance();
+            }
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | InstantiationException e) {
+            throw new BeansException("Failed to instantiate ["+clazz.getName()+"]",e);
+        }
+    }
 }
